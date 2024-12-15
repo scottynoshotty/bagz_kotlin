306510e03d3092a23054a3744ca52a7f91d79cb9
scottynoshotty <scottykampen@gmail.com>
2024-12-14 20:32:02 -0800

    Update to Bagz

--- composeApp/src/commonMain/kotlin/com/eridiumcorp/bagz/App.kt
+++ composeApp/src/commonMain/kotlin/com/eridiumcorp/bagz/App.kt
@@ -1,71 +1,12 @@
 package com.eridiumcorp.bagz
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.eridiumcorp.bagz.app.models.countries
-import com.eridiumcorp.bagz.app.services.currentTimeAt
+import com.eridiumcorp.bagz.components.Bagz
 
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 
 @Composable
 @Preview
-fun App() {
-    MaterialTheme {
-        var showCountries by remember { mutableStateOf(false) }
-        var timeAtLocation by remember { mutableStateOf("No location selected") }
-
-        Column(
-            modifier = Modifier.fillMaxHeight().fillMaxWidth().padding(20.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                timeAtLocation,
-                style = TextStyle(fontSize = 20.sp),
-                textAlign = TextAlign.Center,
-                modifier = Modifier.fillMaxWidth().align(
-                    Alignment.CenterHorizontally
-                )
-            )
-            Row(modifier = Modifier.padding(start = 20.dp, top = 10.dp)) {
-                DropdownMenu(
-                    expanded = showCountries,
-                    onDismissRequest = { showCountries = false }
-                ) {
-                    countries().map {
-                        DropdownMenuItem(
-                            onClick = {
-                                timeAtLocation = currentTimeAt(it.name, it.zone)
-                                showCountries = false
-                            }
-                        ) {
-                            Text(it.name)
-                        }
-                    }
-                }
-            }
-            Button(
-                modifier = Modifier.padding(start = 20.dp, top = 10.dp),
-                onClick = { showCountries = !showCountries }) {
-                Text("Select Location")
-            }
-        }
-    }
-}
+fun App() = MaterialTheme { Bagz() }
--- composeApp/src/commonMain/kotlin/com/eridiumcorp/bagz/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.eridiumcorp.bagz
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
