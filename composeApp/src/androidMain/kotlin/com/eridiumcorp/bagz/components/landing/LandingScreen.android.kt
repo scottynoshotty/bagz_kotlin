5473c11683807ee1e6b964a23bafe45a2863fe6f
scottynoshotty <scottykampen@gmail.com>
2024-12-24 15:40:04 -0800

    Remove loader from landing screen.

--- composeApp/src/androidMain/kotlin/com/eridiumcorp/bagz/components/landing/LandingScreen.android.kt
+++ composeApp/src/androidMain/kotlin/com/eridiumcorp/bagz/components/landing/LandingScreen.android.kt
@@ -1,12 +1,16 @@
 package com.eridiumcorp.bagz.components.landing
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import com.eridiumcorp.bagz.components.home.HomeScreen
 import com.eridiumcorp.bagz.components.signin.SignInScreen
 import org.koin.androidx.compose.koinViewModel
@@ -17,9 +21,21 @@ actual fun LandingScreen(modifier: Modifier) {
     val uiState = landingViewModel.uiState.collectAsState()
     when {
         uiState.value.loading -> {
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                CircularProgressIndicator()
-            }
+            Column(
+                modifier = modifier
+                    .padding()
+                    .fillMaxSize()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                MaterialTheme.colorScheme.primary,
+                                MaterialTheme.colorScheme.secondary
+                            )
+                        )
+                    ),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {}
         }
 
         uiState.value.user == null -> SignInScreen(modifier)
