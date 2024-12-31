707d5eaf94c42be2e4cbcc430db51e124d8f2794
scottynoshotty <scottykampen@gmail.com>
2024-12-30 21:45:29 -0800

    Add top app bar.

--- composeApp/src/androidMain/kotlin/com/eridiumcorp/bagz/components/home/screen/HomeScreen.android.kt
+++ composeApp/src/androidMain/kotlin/com/eridiumcorp/bagz/components/home/screen/HomeScreen.android.kt
@@ -1,5 +1,6 @@
 package com.eridiumcorp.bagz.components.home.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
@@ -18,13 +20,19 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ExitToApp
 import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import org.jetbrains.compose.resources.painterResource
 import androidx.compose.ui.unit.dp
+import bagz.composeapp.generated.resources.Res
+import bagz.composeapp.generated.resources.money_bag_outline_white
 import com.eridiumcorp.bagz.components.LocalNavController
 import com.eridiumcorp.bagz.components.home.widgets.accounts.AccountsWidget
 import com.eridiumcorp.bagz.components.home.widgets.activity.ActivityWidget
@@ -32,12 +40,28 @@ import com.eridiumcorp.bagz.components.home.widgets.bag.BagWidget
 import com.eridiumcorp.bagz.components.link.LinkHost
 import org.koin.androidx.compose.koinViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(viewModel: HomeViewModel = koinViewModel(), modifier: Modifier = Modifier) {
     val navController = LocalNavController.current
     val uiState by viewModel.uiState.collectAsState()
     Scaffold(
         modifier = modifier,
+        topBar = {
+            TopAppBar(
+                expandedHeight = 42.dp,
+                title = {
+                    Image(
+                        painter = painterResource(Res.drawable.money_bag_outline_white),
+                        contentDescription = "App Logo",
+                        modifier = Modifier.size(38.dp)
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                )
+            )
+        },
         floatingActionButton = {
             Row(
                 horizontalArrangement = Arrangement.SpaceBetween,
