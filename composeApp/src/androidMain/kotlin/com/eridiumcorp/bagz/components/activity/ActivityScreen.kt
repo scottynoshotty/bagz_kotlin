4c81cad87e212d3fdf809c52a3d0aa476138ecbd
scottynoshotty <scottykampen@gmail.com>
2024-12-31 12:27:53 -0800

    Add subtypes for Activity.

--- composeApp/src/androidMain/kotlin/com/eridiumcorp/bagz/components/activity/ActivityScreen.kt
+++ composeApp/src/androidMain/kotlin/com/eridiumcorp/bagz/components/activity/ActivityScreen.kt
@@ -1,9 +1,12 @@
 package com.eridiumcorp.bagz.components.activity
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import org.koin.androidx.compose.koinViewModel
 
@@ -12,7 +15,17 @@ fun ActivityScreen(
     activityScreenViewModel: ActivityScreenViewModel = koinViewModel(),
     modifier: Modifier = Modifier,
 ) {
+    val uiState by activityScreenViewModel.uiState.collectAsState()
     Scaffold(modifier = modifier) { padding ->
-        Text(modifier = Modifier.padding(padding), text = "Hi")
+        Column(modifier = Modifier.padding(padding)) {
+            if (uiState.activity?.activityData != null) {
+                val activityData = uiState.activity!!.activityData!!
+                for (activitySummary in activityData) {
+                    Text("${activitySummary.type.name}: ${activitySummary.amount}")
+                }
+            } else {
+                Text("No activity data")
+            }
+        }
     }
 }
