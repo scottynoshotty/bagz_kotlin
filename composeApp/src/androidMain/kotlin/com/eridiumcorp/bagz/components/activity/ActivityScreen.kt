1443d56bb8dc3359936ee203c0672cf31dc9f57c
scottynoshotty <scottykampen@gmail.com>
2025-01-01 19:15:54 -0800

    Add app bar to activity screen.

--- composeApp/src/androidMain/kotlin/com/eridiumcorp/bagz/components/activity/ActivityScreen.kt
+++ composeApp/src/androidMain/kotlin/com/eridiumcorp/bagz/components/activity/ActivityScreen.kt
@@ -2,21 +2,39 @@ package com.eridiumcorp.bagz.components.activity
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.koin.androidx.compose.koinViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ActivityScreen(
     activityScreenViewModel: ActivityScreenViewModel = koinViewModel(),
     modifier: Modifier = Modifier,
 ) {
     val uiState by activityScreenViewModel.uiState.collectAsState()
-    Scaffold(modifier = modifier) { padding ->
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                expandedHeight = 42.dp,
+                title = {
+                    Text("Your activity")
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                )
+            )
+        },
+        modifier = modifier) { padding ->
         Column(modifier = Modifier.padding(padding)) {
             if (uiState.activity?.activityData != null) {
                 val activityData = uiState.activity!!.activityData!!
