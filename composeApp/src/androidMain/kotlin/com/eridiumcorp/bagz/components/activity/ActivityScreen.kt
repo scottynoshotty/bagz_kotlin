03fb015f305c57dfef80d38aa704dfbc79e96d26
scottynoshotty <scottykampen@gmail.com>
2025-01-01 19:23:00 -0800

    Update ActivityScreen UI.

--- composeApp/src/androidMain/kotlin/com/eridiumcorp/bagz/components/activity/ActivityScreen.kt
+++ composeApp/src/androidMain/kotlin/com/eridiumcorp/bagz/components/activity/ActivityScreen.kt
@@ -2,7 +2,11 @@ package com.eridiumcorp.bagz.components.activity
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -12,7 +16,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import org.koin.androidx.compose.koinViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -25,23 +28,41 @@ fun ActivityScreen(
     Scaffold(
         topBar = {
             TopAppBar(
-                expandedHeight = 42.dp,
-                title = {
-                    Text("Your activity")
-                },
+                title = { Text("Your activity") },
                 colors = TopAppBarDefaults.topAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer
                 )
             )
         },
-        modifier = modifier) { padding ->
-        Column(modifier = Modifier.padding(padding)) {
+        modifier = modifier
+    ) { padding ->
+        val scrollState = rememberScrollState()
+        Column(modifier = Modifier
+            .padding(padding)
+            .verticalScroll(scrollState)) {
             if (uiState.activity?.activityData != null) {
                 val activityData = uiState.activity!!.activityData!!
                 for (activitySummary in activityData) {
-                    Text("${activitySummary.primaryActivitySummary.type.name}: ${activitySummary.primaryActivitySummary.amount}")
+                    ListItem(
+                        headlineContent = {
+                            Text(
+                                "${activitySummary.primaryActivitySummary.type.name}: ${activitySummary.primaryActivitySummary.amount}",
+                                style = MaterialTheme.typography.headlineSmall
+                            )
+                        },
+                        colors = ListItemDefaults.colors(
+                            containerColor = MaterialTheme.colorScheme.primaryContainer
+                        )
+                    )
                     for (detailedActivitySummary in activitySummary.detailedActivitySummaries) {
-                        Text("${detailedActivitySummary.type.name}: ${detailedActivitySummary.amount}")
+                        ListItem(
+                            headlineContent = {
+                                Text(
+                                    "${detailedActivitySummary.type.name}: ${detailedActivitySummary.amount}",
+                                    style = MaterialTheme.typography.bodyLarge
+                                )
+                            }
+                        )
                     }
                 }
             } else {
