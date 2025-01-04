3203baa44bb2a37b2dcb0d742f10e2b9a37f1c56
scottynoshotty <scottykampen@gmail.com>
2025-01-03 21:02:24 -0800

    Update link host screen to M3.

--- composeApp/src/androidMain/kotlin/com/eridiumcorp/bagz/components/link/LinkHostScreen.android.kt
+++ composeApp/src/androidMain/kotlin/com/eridiumcorp/bagz/components/link/LinkHostScreen.android.kt
@@ -6,9 +6,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
